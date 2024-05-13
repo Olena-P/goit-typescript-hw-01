edbb679d0a95e4600a5bca74f2a0099763cc909c
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2024-05-13 19:47:42 +0300

    Implement non-mutating merge using Object.assign

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,9 @@
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function merge<T, U>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
+
+const obj1 = { a: 1, b: true };
+const obj2 = { b: false, c: "test" };
+
+const result = merge(obj1, obj2);
+console.log(result);
